7465f97cfa86ee4a0921a5fc83cc3210a01048e2
akwasin <akwasiijunior@gmail.com>
2016-05-18 09:42:41 +0200

    2 ajax links

---
+++ AjaxWebApplication/Controllers/CarController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AjaxWebApplication.Models;
+
+namespace AjaxWebApplication.Controllers
+{
+    public class CarController : Controller
+    {
+        // GET: Car
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult _Create(int id)
+        {
+            Car car = new Car {PersonId = id};
+            return PartialView(viewName: "_Create", model: car);
+        }
+    }
+}
---
+++ AjaxWebApplication/Models/Car.cs
@@ -0,0 +1,9 @@
+﻿namespace AjaxWebApplication.Models
+{
+    public class Car
+    {
+        public int PersonId { get; set; }
+        public string Color { get; set; }
+        public string Brand { get; set; } 
+    }
+}
