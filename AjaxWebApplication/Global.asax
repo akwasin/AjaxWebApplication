8c1057de83703b3d9741b3dd8118f25ba9c7ea05
akwasin <akwasiijunior@gmail.com>
2016-05-18 08:56:36 +0200

    Add project files.

---
+++ AjaxWebApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AjaxWebApplication.MvcApplication" Language="C#" %>
---
+++ AjaxWebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AjaxWebApplication.Startup))]
+namespace AjaxWebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
